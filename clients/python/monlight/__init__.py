c24a681aa650cc4d49992e6d370be6c8a94f9a18
Matteo Merola <mattmezza@gmail.com>
2026-04-09 15:50:57 +0200

    release: monlight (python) v0.3.0

--- clients/python/monlight/__init__.py
+++ clients/python/monlight/__init__.py
@@ -3,5 +3,5 @@
 from monlight.error_client import ErrorClient
 from monlight.metrics_client import MetricsClient
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 __all__ = ["ErrorClient", "MetricsClient"]
